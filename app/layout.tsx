8ddca23e793f65204853edb42095ffc66bab2173
Elijah Crisehea Nollen <emnollen@student.apc.edu.ph>
2026-07-29 22:10:18 +0800

    Load social profile display fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./fixes.css";
 
 export const metadata: Metadata = {
   title: "José Rizal | Personal & Professional Social Profiles",
